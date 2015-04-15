032f0bc253d3835512afc45cf53aa9cb20c2fa07
Patrick Lo <patricklo87@gmail.com>
2015-04-14 23:58:34 -0700

    key highlight complete

--- pianoDemo/lib/piano.js
+++ pianoDemo/lib/piano.js
@@ -2,11 +2,12 @@ $(document).ready(function () {
   $('.white-key').click(function (e) {
     var $currentTarget = $(e.currentTarget)
     var key = $currentTarget.data('key');
+    $('.key-log').append(key);
     console.log(key);
   });
 
   $('.black-key').click(function (e) {
     e.stopPropagation();
-  })
+  });
 
 })
