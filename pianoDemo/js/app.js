7d3af7ab66391a4b75147cb88fac30fe8c634d2c
Patrick Lo <patricklo87@gmail.com>
2015-04-16 22:21:43 -0700

    input field working

--- pianoDemo/js/app.js
+++ pianoDemo/js/app.js
@@ -9,4 +9,40 @@ $(document).ready(function () {
   $('.black-key').click(function (e) {
     e.stopPropagation();
   });
+
+  $('.key-input').keypress(function (e) {
+    if (e.which == '13') {
+      var input = $('.key-input').val();
+      var formattedArr = filterKeys(input);
+      var time = 1000;
+      var i = 0;
+      var keyInterval = setInterval(function() {
+        if (i < formattedArr.length) {
+          $('.highlighted').toggleClass('highlighted');
+          var $target = $('[data-key="' + formattedArr[i] + '"]');
+          $target.toggleClass('highlighted');
+          i++;
+        } else {
+          $('.highlighted').toggleClass('highlighted');
+          clearInterval(keyInterval);
+        }
+      }, 1000);
+      $('.key-input').val('');
+    }
+  })
 })
+
+function filterKeys (keys) {
+  var validKeys = "cdefgab";
+  var filteredKeys = [];
+  var splitKeys = keys.replace(/[, ]/g, '').split('');
+  splitKeys.forEach(function (char) {
+    if (validKeys.indexOf(char) != -1) {
+      filteredKeys.push(char);
+    }
+  });
+
+  return filteredKeys;
+}
+
+filterKeys('f, d, c,c d')
